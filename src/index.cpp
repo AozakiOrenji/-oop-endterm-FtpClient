5737068d9c5c6eb374421a4019440de02d8317be
苍崎橙子 <AozakiOrenji@outlook.com>
2017-06-20 19:16:43 +0800

    mv main.cpp src/index.cpp

---
+++ src/index.cpp
@@ -0,0 +1,10 @@
+/*
+ * This file was created by Aozak using CLion 2017.2 EAP as my end-term OOP project at Jun 20 2017.
+ */
+
+#include "functions.h"
+
+int main(){
+    objMenu objMenu1;
+    objMenu1.print();
+}
